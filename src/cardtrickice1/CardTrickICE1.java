e494c7ee8cdbe09ff8498e6fffd96f3fc36aae36
amanjeetk4 <kau11747@sheridancollege.ca>
2022-06-01 22:12:07 -0400

    AFTER COMMIT

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -1,9 +1,12 @@
-/*
+/*AMANJEET KAUR BOLA    STUDENT ID:991660553
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package cardtrickice1;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /** step1 : generate 7 random cards and store in array - how
  * step 2: take any card input from user suit,number
@@ -18,16 +21,39 @@ public class CardTrickICE1 {
      */
     public static void main(String[] args) 
     {
+        
         Card[] magicHand = new Card[7]; //Array of object
-        for( int i=0;i<magicHand.length;i++)
-        {
+        
+System.out.println("Please choose any card");
+//Random number=new Random();
+
+        for (int i=0; i<magicHand.length;i++) {
             Card c1 = new Card();
-            c1.setValue(2);//use a method to generate random *13
+            
+            //c1.setValue(int)(Math.random()*13+1); //use a method to generate random *13
             c1.setSuits("hearts");//random method suit 
-        }
-        //step 2:take input 
+        
+        
+
+//step 2:take input 
+    magicHand[i] = c1;
+    System.out.println(c1);
         
         //step 3: match with array 
-    }
+        Scanner input=new Scanner(System.in);
+    System.out.println("Enter suit: ");
+    String suit=input.nextLine();
     
-}
+    System.out.println("Enter any number: ");
+    int value=input.nextInt();
+  /*String suit=input.next();
+    Card key=new Card();
+    key.setValue(value);
+    key.getSuits();*/
+        }
+    
+    
+    
+    }}
+    
+
